b538dfc2af45b49d2c02885a22b5ef5064863f04
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-10-03 00:51:53 +0530

    resolves #7: using absolute path to render relative path.

--- cmd/restatic/main.go
+++ cmd/restatic/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"flag"
+	"path/filepath"
 
+	"github.com/relogHQ/restatic/config"
 	"github.com/relogHQ/restatic/server"
 )
 
 func main() {
 	flag.Parse()
+	if bPath, err := filepath.Abs(config.Directory); err == nil {
+		config.Directory = bPath
+	}
 	server.Run()
 }
